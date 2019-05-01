c09b0f0490d27601c9adc55d5a24c5e0dfd3a059
Christopher R. Wren <c@drwren.com>
2019-05-01 07:41:14 -0400

    add copyright

--- bluelight.py
+++ bluelight.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+
+# Copyright 2019 Chris Wren
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Heavily copied from cloudiot_mqtt_example.pp
+# https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/iot/api-client/mqtt_example/cloudiot_mqtt_example.py
+
 # [START iot_mqtt_includes]
 import argparse
 import datetime
